a5a41cbad4274f602c9f14b4ca93c17f0aaae749
NeutralDread <jordanamiles@hotmail.com>
2019-06-22 18:49:51 -0500

    Removed default comments

--- React User Registration/user-auth/src/index.js
+++ React User Registration/user-auth/src/index.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import store from './stores'
-import { Provider } from 'react-redux'
-import Intro from './components/Intro'
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * *
-	This is the entry point of the React app with Redux
-	already implemented. The Intro component is the
-	visual content and most likely, you will want
-	to remove it and replace with your own visual content.
-* * * * * * * * * * * * * * * * * * * * * * * * * * * *
-*/
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import store from './stores';
+import { Provider } from 'react-redux';
+import Intro from './components/Intro';
 
 const app = (
-	<Provider store={store.configure(null)}>
-		<Intro />
-	</Provider>
-)
+  <Provider store={store.configure(null)}>
+    <Intro />
+  </Provider>
+);
 
-ReactDOM.hydrate(app, document.getElementById('root'))
+ReactDOM.hydrate(app, document.getElementById('root'));
